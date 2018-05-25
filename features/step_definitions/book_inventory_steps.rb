39dd2ab6b04806d352568f98c70057f3d0698c49
Olatunbosun <tunbosunogunlana1@gmail.com>
2018-05-25 17:59:20 +0100

    Describe book inventory listing

---
+++ features/step_definitions/book_inventory_steps.rb
@@ -0,0 +1,9 @@
+Given("I have populated my inventory with several books") do
+  FactoryBot.create(:book, :user => @registered_user, :name => 'The Punisher', :author => 'John Doe')
+  FactoryBot.create(:book, :user => @registered_user, :name => 'Moby Dick', :author => 'Herman Melville')
+end
+
+Then("I should see the list of my books") do
+  expect(page).to have_content('The Punisher')
+  expect(page).to have_content('Moby DIck')
+end
